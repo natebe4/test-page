a25d842a3d6dd152c3630f96fe53c9f743789745
natebe4 <167240672+natebe4@users.noreply.github.com>
2024-05-11 03:40:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("JavaScript file loaded");
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('designs').style.display = 'none';
+    document.getElementById('codingButton').addEventListener('click', function () {
+        document.getElementById('coding').style.display = 'grid';
+        document.getElementById('designs').style.display = 'none';
+    });
+
+    document.getElementById('designButton').addEventListener('click', function () {
+        document.getElementById('designs').style.display = 'block';
+        document.getElementById('coding').style.display = 'none';
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var getInTouchButton = document.getElementById('get-in-touch');
+    var form = document.getElementById('myForm');
+    getInTouchButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        if (form.style.display === 'none') {
+            form.style.display = 'block';
+        } else {
+            form.style.display = 'none';
+        }
+    });
+});
